4ba0bdf953bbd919b35fb3e1a4d13d5ac69a9e00
vadimgupal <114927468+vadimgupal@users.noreply.github.com>
2026-04-05 14:16:09 +0300

    fix: update exception

--- core/src/main/java/core/DTO/TokenExchangeException.java
+++ core/src/main/java/core/DTO/TokenExchangeException.java
@@ -1,6 +1,16 @@
 package core.DTO;
 
+import lombok.Getter;
+
+@Getter
 public class TokenExchangeException extends RuntimeException {
+    private boolean invalidGrant;
+
+    public TokenExchangeException(String message, boolean invalidGrant) {
+        super(message);
+        this.invalidGrant = invalidGrant;
+    }
+
     public TokenExchangeException(String message) {
         super(message);
     }

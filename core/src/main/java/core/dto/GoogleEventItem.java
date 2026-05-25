5f42fb694f5039fdc7b680526110be9ec2a653a6
vadimgupal <114927468+vadimgupal@users.noreply.github.com>
2026-05-26 01:20:42 +0300

    feat: add new fields

--- core/src/main/java/core/dto/GoogleEventItem.java
+++ core/src/main/java/core/dto/GoogleEventItem.java
@@ -1,12 +1,15 @@
-package core.DTO;
+package core.dto;
 
 import java.util.List;
+import java.util.Map;
 
 public record GoogleEventItem(String id,
+                              String status,
                               String summary,
                               String description,
                               GoogleEventDateTime start,
                               GoogleEventDateTime end,
                               String location,
+                              Map<String, Object> reminders,
                               List<String> recurrence) {
 }
